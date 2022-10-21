eec0d92f860e05b4091ce7e6d37ce59015cdc231
Triton-Z <98340562+Triton-Z@users.noreply.github.com>
2022-10-21 15:01:32 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-const char = document.createElement("p");
+const createGameCanvas = () => 
+{
+  for (let i = 1; i<200; i++)
+  {
+    const char = document.createElement("p");
+    char.setAttribute("id", i);
+    document.getELementById(i).innerHTML = "#"
+  }
+}
